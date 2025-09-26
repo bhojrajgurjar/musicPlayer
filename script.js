1c133fbaf1a8ad96010306190b774616cb32c180
bhojrajgurjar <bgurjar2000@gmail.com>
2025-09-26 18:18:26 +0530

    func add

--- script.js
+++ script.js
@@ -5,72 +5,88 @@ let source = document.getElementById("source");
 let title = document.getElementById("title");
 let singer = document.getElementById("singer");
 
-song.onloadedmetadata = function(){
-    progress.max = song.duration;
-    progress.value = song.currentTime;
+const songs = [
+    {title:"Despacito", singer:"Luis Fonsi ft. Puerto Rican", url:"media/music/despacito.mp3"},
+    {title:"Afsana Banake", singer:"", url:"media/music/AfsanaBanake.mp3"},
+    {title:"Ashiqana Alam", singer:"", url:"media/music/AshiqanaAlam.mp3"},
+    {title:"Ashiq Banaya", singer:"", url:"media/music/AshiqBanaya.mp3"},
+    {title:"Baaton ko Teri", singer:"", url:"media/music/BaatonKoTeri.mp3"},
+    {title:"Bekhudi", singer:"", url:"media/music/Bekhudi.mp3"},
+    {title:"Bewajah", singer:"", url:"media/music/Bewajah.mp3"},
+    {title:"Bhool Bhulaiya Title", singer:"", url:"media/music/BhoolBhulaiyaa3.mp3"},
+    {title:"Bichdann", singer:"", url:"media/music/Bichdann.mp3"},
+    {title:"Kashish", singer:"", url:"media/music/kashish.mp3"},
+    {title:"Kissik Pushpa 2", singer:"", url:"media/music/KissikPushpa2.mp3"},
+    {title:"Peelings Pushpa 2", singer:"", url:"media/music/PeelingsPushpa2.mp3"},
+    {title:"Singham Again Title", singer:"", url:"media/music/SinghamAgain.mp3"} 
+];
+
+let currentSongIndex = 0;
+
+// Load a song and start playing
+function loadSong(index) {
+    source.src = songs[index].url;
+    song.load();
+    song.play();
+    title.textContent = songs[index].title;
+    singer.textContent = songs[index].singer;
+    controlIcon.classList.remove("fa-play");
+    controlIcon.classList.add("fa-pause");
 }
 
-function playPause(){
-    if(controlIcon.classList.contains("fa-pause")){
+// Play/Pause toggle
+function playPause() {
+    if (controlIcon.classList.contains("fa-pause")) {
         song.pause();
         controlIcon.classList.remove("fa-pause");
         controlIcon.classList.add("fa-play");
-    }else{
+    } else {
         song.play();
         controlIcon.classList.remove("fa-play");
         controlIcon.classList.add("fa-pause");
     }
 }
 
-if(song.play()){
-    setInterval(()=>{
-        progress.value = song.currentTime;
-    },500)
-}
+// Update progress bar while playing
+song.addEventListener("timeupdate", () => {
+    progress.max = song.duration;
+    progress.value = song.currentTime;
+});
 
-progress.onchange = function(){
-    song.play();
+// Seek in song
+progress.onchange = function () {
     song.currentTime = progress.value;
-    controlIcon.classList.remove("fa-play");
-    controlIcon.classList.add("fa-pause");
-}
-
-
-const songs = [
-    {title:"Despacito",singer:"Luis Fonsi ft. Puerto Rican",url:"media/music/despacito.mp3"},
-    {title:"Afsana Banake",singer:"",url:"media/music/AfsanaBanake.mp3"},
-    {title:"Ashiqana Alam",singer:"",url:"media/music/AshiqanaAlam.mp3"},
-    {title:"Ashiq Banaya",singer:"",url:"media/music/AshiqBanaya.mp3"},
-    {title:"Baaton ko Teri",singer:"",url:"media/music/BaatonKoTeri.mp3"},
-    {title:"Bekhudi",singer:"",url:"media/music/Bekhudi.mp3"},
-    {title:"Bewajah",singer:"",url:"media/music/Bewajah.mp3"},
-    {title:"Bhool Bhulaiya Title",singer:"",url:"media/music/BhoolBhulaiyaa3.mp3"},
-    {title:"Bichdann",singer:"",url:"media/music/Bichdann.mp3"},
-    {title:"Kashish",singer:"",url:"media/music/kashish.mp3"},
-    {title:"Kissik Pushpa 2",singer:"",url:"media/music/KissikPushpa2.mp3"},
-    {title:"Peelings Pushpa 2",singer:"",url:"media/music/PeelingsPushpa2.mp3"},
-    {title:"Singham Again Title",singer:"",url:"media/music/SinghamAgain.mp3"} 
-]
-
-let currentSongIndex=0;
-
-function loadSong(index){
-    source.src = songs[index].url;
-    song.load();
     song.play();
-    title.textContent = `${songs[index].title}`;
-    singer.textContent = `${songs[index].singer}`;
     controlIcon.classList.remove("fa-play");
     controlIcon.classList.add("fa-pause");
-}
+};
 
-function nextSong(){
-    currentSongIndex = (currentSongIndex+1) % songs.length;
+// Next / Previous song
+function nextSong() {
+    currentSongIndex = (currentSongIndex + 1) % songs.length;
     loadSong(currentSongIndex);
 }
-function prevSong(){
-    currentSongIndex = (currentSongIndex-1)%songs.length;
+function prevSong() {
+    currentSongIndex = (currentSongIndex - 1 + songs.length) % songs.length;
     loadSong(currentSongIndex);
 }
 
-loadSong(currentSongIndex);
+// Auto play next when current ends
+song.addEventListener("ended", nextSong);
+
+// Keyboard controls
+document.addEventListener("keydown", (event) => {
+    if (event.key === "ArrowRight") {
+        song.currentTime += 10;
+    } else if (event.key === "ArrowLeft") {
+        song.currentTime -= 10;
+    } else if (event.code === "Space") {
+        event.preventDefault();
+        playPause();
+    }
+});
+
+// Load first song automatically on page load
+window.onload = function () {
+    loadSong(currentSongIndex);
+};
